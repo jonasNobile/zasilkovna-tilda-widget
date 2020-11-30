5bc268b42d9323741ee3ba466c2e70bf90fbc22a
Jonáš Nobile <jonas@netvarec.ooo>
2020-11-30 23:04:15 +0100

    Dev: refactoring

---
+++ src/utils/saveSelectedPickupPoint.ts
@@ -0,0 +1,9 @@
+export function saveSelectedPickupPoint(point) {
+    var zasilkovnaOutput: any = document.querySelector('[name="packeta-point-id"]'); // check the real name of input
+  
+    if (point) {
+      zasilkovnaOutput.value = "id: ".concat(point.id, ", place: ").concat(point.place, ", url: ").concat(point.url);
+    } else {
+      zasilkovnaOutput.value = "None";
+    }
+  }
---
+++ src/zasilkovna-tilda-widget.ts
@@ -0,0 +1,16 @@
+import { saveSelectedPickupPoint } from "./utils/saveSelectedPickupPoint";
+
+let Packeta: { 'Widget' };
+
+(function () {
+  const apiKey = document.currentScript.getAttribute("apiKey");
+  window.onload = () => {
+    const zasilkovnaButton = document.querySelector('[name*="Zasilkovna"]'); // check the real name of input
+    
+    zasilkovnaButton.addEventListener(
+      "click",
+      () => Packeta.Widget.pick(apiKey, saveSelectedPickupPoint),
+      false
+    );
+  };
+})();

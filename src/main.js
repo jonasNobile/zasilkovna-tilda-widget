3cdb806cd0eca650dc046326a4331fcfc542bb8c
Jonáš Nobile <jonas@netvarec.ooo>
2020-12-02 23:02:45 +0100

    Fix: correct name of zasilkovna button

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ let Packeta;
 (function () {
   const apiKey = document.currentScript.getAttribute("apiKey");
   window.onload = () => {
-    const zasilkovnaButton = document.querySelector('[name*="Zasilkovna"]'); // check the real name of input
+    const zasilkovnaButton = document.querySelector('[name="Delivery"][value*="Zásilkovna"]');
 
     if (zasilkovnaButton) {
       zasilkovnaButton.addEventListener(

fca77949755e6794ea100bb74cd1c85c6968914a
jonasNobile <jonas.nobile@icloud.com>
2021-03-12 19:31:32 +0100

    🐞fix: Better onload function

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 import { saveSelectedPickupPoint } from "./utils/saveSelectedPickupPoint";
-(function () {
-  const apiKey = document.currentScript.getAttribute("apiKey");
-  window.onload = () => {
-    const zasilkovnaButton = document.querySelector('[value*="Zásilkovna"]');
-    const zasilkovnaOutput = document.querySelector('[name="packeta-point-id"]');
 
-    if (zasilkovnaButton) {
-      zasilkovnaButton.addEventListener(
-        "change",
-        () => {
-          Packeta.Widget.pick(apiKey, (point) => {
-            saveSelectedPickupPoint(point, zasilkovnaButton, zasilkovnaOutput);
-          });
-        },
-        false
-      );
-    }
-  };
-})();
+const apiKey = document.currentScript.getAttribute("apiKey");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const zasilkovnaButton = document.querySelector('[value*="Zásilkovna"]');
+  const zasilkovnaOutput = document.querySelector('[name="packeta-point-id"]');
+
+  if (zasilkovnaButton) {
+    zasilkovnaButton.addEventListener(
+      "change",
+      () => {
+        Packeta.Widget.pick(apiKey, (point) => {
+          saveSelectedPickupPoint(point, zasilkovnaButton, zasilkovnaOutput);
+        });
+      },
+      false
+    );
+  }
+});
